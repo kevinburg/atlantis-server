c072fd7a5bb30ff4ab8abf14f43e7e5568a6f75e
pmsdev <pmsdev@pmsdev-VirtualBox.(none)>
2014-02-01 04:35:24 -0500

    some basis for server functions, trying http

--- app.js
+++ app.js
@@ -96,6 +96,7 @@ app.get('/adduser/:id', function(req, res) {
 
   http.request(options, callback).end();
 */
+  var str = '';
   https.get("https://apis.scottylabs.org/v1/directory/andrewid/rparen?app_id=4dc26847-3962-47a6-aa50-dcd650e900b1&app_secret_key=_gH91EeosouyjtswFjR3SsmmCJkOWF93Lxb2LO1qdieZTpUqToYxGX4k", function(res1) {
    res1.on('data', function (chunk) {
      str += chunk;
