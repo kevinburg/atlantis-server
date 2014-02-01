b148e948272a8511da563a828fea00665cf6e9fa
Kevin Burg <kburg@andrew.cmu.edu>
2014-01-31 19:28:30 -0500

    Mock login form.

--- app.js
+++ app.js
@@ -12,7 +12,26 @@ var mongoUri = process.env.MONGOLAB_URI ||
 var db = monk(mongoUri);
 
 app.get('/', function(req, res) {
-  res.send("Hello world!");
+  var users = db.get('users');
+  users.find({}, {}, function(e, docs) {
+    res.send(docs);
+  });
+});
+
+app.post('/login', function(req, res) {
+  var users = db.get('users');
+  var query = {id : req.body.id};
+  console.log('Login request received for id:', req.body.id);
+  users.find(query, {}, function(e, docs) {
+    if (docs.length == 0) {
+      var object = {id : req.body.id, info : req.body.info};
+      users.insert(object, {safe : true}, function(err, records) {
+	res.send(object);
+      });
+    } else {
+      res.send({'ok' : 'ok'});
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000);
